01aab3c81732aa427abaabd6981cb5f328884d5e
kanchan-solanke <kanchanssolanke@gmail.com>
2021-07-15 08:32:15 +0530

    draw a  box

---
+++ TicTacToeGameUC1.java
@@ -0,0 +1,14 @@
+public class TicTacToeGameUC1 {
+
+    private static String num;
+    char[] ch = new char[10];
+
+    public static void main(String args[]) {
+        System.out.println("|" + "[1]" + "|" + "[2]" + "|" + "[3]" + "|");
+        System.out.println("------------------");
+        System.out.println("|" + "[4]" + "|" + "[5]" + "|" + "[6]" + "|");
+        System.out.println("------------------");
+        System.out.println("|" + "[7]" + "|" + "[8]" + "|" + "[9]" + "|");
+        System.out.println("------------------");
+    }
+}
