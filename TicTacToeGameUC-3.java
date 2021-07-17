453afa5c2cfb4cd88b4ca74b5f0f9fe0b9593477
kanchan-solanke <kanchanssolanke@gmail.com>
2021-07-17 22:35:55 +0530

    choose the valid cell to make a move

---
+++ TicTacToeGameUC-3.java
@@ -0,0 +1,61 @@
+package com.company;
+import java.util.*;
+public class TicTacToeUC3
+{
+    public char computersymbol;
+    public String playersymbol;
+    public void showBoard(char[] board)
+    {
+        System.out.println("Current board:");
+        int count=0;
+        System.out.println("--------------");
+        for(int index=1;index<board.length;index++)
+        {
+
+            System.out.print(board[index]+" | ");
+            if(count==2 || count==5 || count==8)
+            {
+                System.out.println();
+                System.out.println("--------------");
+            }
+
+            count=count+1;
+        }
+
+    }
+    public void determinesymbol()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your Symbol:");
+        String symbol = sc.next();
+        if  (symbol.toUpperCase().equals("X"))
+        {
+            playersymbol=symbol;
+            computersymbol='O';
+            System.out.println("player:"+playersymbol+" computer:"+computersymbol);
+        }
+        else if (symbol.toUpperCase().equals("O"))
+        {
+            playersymbol=symbol;
+            computersymbol='X';
+            System.out.println("player:"+playersymbol+" computer:"+computersymbol);
+        }
+        else
+        {
+            System.out.println("You have entered character other than X or O");
+        }
+
+    }
+    public static void main(String[] args)
+    {
+        char[] board = new char [10];
+        for(int index=0;index<10;index++)
+        {
+            board[index]=' ';
+        }
+        TicTacToeUC3 obj1 = new TicTacToeUC3();
+        obj1.determinesymbol();
+        obj1.showBoard(board);
+    }
+
+}
